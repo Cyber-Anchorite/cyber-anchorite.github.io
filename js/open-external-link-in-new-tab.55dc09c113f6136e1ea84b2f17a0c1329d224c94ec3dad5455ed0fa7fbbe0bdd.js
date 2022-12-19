e9493dadef1eac72d6a5a940b4f541d4b1f7c87e
Cyber Anchorite <cyber-anchorite@proton.me>
2022-12-19 16:39:29 +0000

    在新标签页打开外链

---
+++ js/open-external-link-in-new-tab.55dc09c113f6136e1ea84b2f17a0c1329d224c94ec3dad5455ed0fa7fbbe0bdd.js
@@ -0,0 +1,1 @@
+(()=>{window.onload=function(){var a=document.getElementsByClassName("article-post");if(a.length!==0)for(var t=document.getElementsByClassName("article-post")[0].getElementsByTagName("a"),e=0;e<t.length;e++)t[e].href.indexOf(location.origin)!==0&&t[e].setAttribute("target","_blank")};})();
